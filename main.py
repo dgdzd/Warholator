ec41d498f51330609470123f9e52824a7eb93b2d
dgdzd <113797245+dgdzd@users.noreply.github.com>
2024-10-16 07:43:20 +0200

    small commit

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import core.wahrol as wahrol
 from PIL import Image
 import gui.app as app
 import utils.timetracker as tt
+import random
 
 if __name__ == "__main__":
     app.Window("Wahrolator", (800, 600))
     perf, img = tt.get_performance(wahrol.wahrolate, Image.open('resources/andy.jpg'))
-    img.save("resources/test.png")
+    img.save(f"out/test-{random.randint(1000, 9999)}.png")
     print(perf)
